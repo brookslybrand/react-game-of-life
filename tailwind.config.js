d28ac0ea2c43a3539038441207cf0dd99fbc076c
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2020-08-12 08:25:43 -0500

    Update packages and tailwind purging

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./src/**/*.js', './public/index.html'],
+  purge: ['./public/index.html', './src/**/*.ts', './src/**/*.tsx'],
   theme: {
     fontFamily: {
       sans: ['Poppins', '-apple-system', 'BlinkMacSystemFont'],
